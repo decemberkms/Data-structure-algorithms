a459d0bf3bc76120199b5652ddfd8bd5ab282ca4
decemberkms <decemberkms@gmail.com>
2022-01-05 22:51:29 +0100

    Add till Sorting

--- Udemy_Chapter03_arrayADT/03/main.cpp
+++ Udemy_Chapter03_arrayADT/03/main.cpp
@@ -4,32 +4,16 @@
 int main(){
 
     Array arr(10);
-
     arr.create();
-    // arr.display();
+    arr.display();
+    
+    std::cout << std::endl;
+   
+    std::boolalpha;
+    std::cout << arr.isSorted() << std::endl;
 
-    arr.append(6);
+    arr.negaposisorted();
     arr.display();
 
-    // arr.insert(1, 100);
-    // arr.display();
-
-    // arr.Delete(1);
-    // arr.display();
-
-    // std::cout << "\nResult index: " << arr.linearSearch(6) << std::endl;
-    // std::cout << "Result index: " <<  arr.linearSearch(100) << std::endl;
-
-    // std::cout << "\nResult index: " << arr.binarySearch_loop(0,5,3) << std::endl;
-    // std::cout << "Result index: " <<  arr.binarySearch_recursive(0,5,8) << std::endl;
-    // std::cout <<  arr.Get(0) << std::endl;
-    // arr.Set(0,0);
-    // std::cout <<  arr.Get(0) << std::endl;
-    // std::cout <<  arr.Max() << std::endl;
-    // std::cout <<  arr.Min() << std::endl;
-    // std::cout << arr.Sum() << std::endl;
-    // std::cout << arr.Average() << std::endl;
-    arr.Reverse();
-    arr.display();
     return 0;
 }
---
+++ Udemy_Chapter03_arrayADT/04/main.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+#include "Array.hpp"
+
+int main(){
+
+    Array arr(10);
+    arr.create();
+    arr.display();
+    
+
+    return 0;
+}
